efbeae7e0b49782558c50eaa6f7f11da36ee1100
Satoshi Ogata <satosystems@gmail.com>
2021-03-15 15:17:03 +0900

    Refactor on PR review
    PR レビュー時に以下のリファクタリングを実施。
    
    - ESLint 警告除去
    - 'use strict' 追加
    - デバッグ用関数をグローバルスコープに移動
    - メールアドレス取得処理をグローバルスコープに関数として作成
    - 不要なローカル変数を削除
    - 不要なループを削除
    - メールサブジェクトを問い合わせ内容に応じて変更
    - 不要なコメントを除去
    - その他細かい修正

--- main.js
+++ main.js
@@ -1,53 +1,46 @@
 /* global GmailApp, SpreadsheetApp */
 
+'use strict'
+
+const SPREADSHEET_ID = '1hxE7cBpoPz3o5o2EJtExOCUFH5SFDoOiG-vW1MrJByo'
+
+// eslint-disable-next-line no-unused-vars
+function debug (str) {
+  const ss = SpreadsheetApp.openById(SPREADSHEET_ID)
+  const s = ss.getSheetByName('デバッグログ')
+  s.appendRow([new Date().toLocaleString(), str])
+}
+
+function getMailAddresses () {
+  const s = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName('送信アドレス') // シート取得
+  return s.getDataRange().getValues() // 項目・連絡先が入力されているセルの取得
+}
+
 // eslint-disable-next-line no-unused-vars
 function submitForm (e) {
-  const s = SpreadsheetApp.openById('1hxE7cBpoPz3o5o2EJtExOCUFH5SFDoOiG-vW1MrJByo').getSheetByName('送信アドレス') // シート取得
-  const data = s.getDataRange().getValues() // 項目・連絡先が入力されているセルの取得
-
-  let title = ''
-  let answer = ''
+  let mailAddress = ''
   let content = ''
-  let address = ''
-  let itemResponse = ''
-  let titles = ''
-  let answers = ''
-  const arr0 = []
-  const arr1 = []
+  let mailSubject = ''
 
-  // eslint-disable-next-line no-unused-vars
-  function debug (str) {
-    const ss = SpreadsheetApp.openById('1hxE7cBpoPz3o5o2EJtExOCUFH5SFDoOiG-vW1MrJByo')
-    const s = ss.getSheetByName('デバッグログ')
-    s.appendRow([new Date().toLocaleString(), str])
-  }
+  const mailAddresses = getMailAddresses()
 
   // フォーム入力内容の取得
   const itemResponses = e.response.getItemResponses()
-  for (let i = 0; i < itemResponses.length; i++) {
-    itemResponse = itemResponses[i]
-    title = itemResponse.getItem().getTitle()
-    answer = itemResponse.getResponse()
-    titles = [title.toString()]
-    answers = [answer.toString()]
-    // push メソッドで空の配列に質問内容と回答を追加
-    arr0.push(titles)
-    arr1.push(answers)
-    content += '\n\n[' + title + ']\n\n'
-    content += answer
-  }
-
-  // 入力内容によって、取得したセル値から送信先を選択
-  for (let d = 1; d < data.length; d++) {
-    if (arr0[0] == '相談内容を選択して下さい') {
-      if (arr1[0] == 'ホットライン') {
-        address = data[d][1]
-        break
-      } else if (arr1[0] == 'コンプライアンス') {
-        address = data[d][1]
+  for (const itemResponse of itemResponses) {
+    const title = itemResponse.getItem().getTitle()
+    const answer = itemResponse.getResponse()
+    content = `${content}[${title}]\n${answer}\n\n`
+    // 送信先メールアドレスを解決する
+    if (title === '相談内容を選択して下さい') {
+      for (const mailAddressPair of mailAddresses) {
+        if (answer === mailAddressPair[0]) {
+          mailAddress = mailAddressPair[1]
+          mailSubject = `【${answer}】に関する相談`
+          break
+        }
       }
     }
   }
-  title = 'お問い合わせが送信されました'
-  GmailApp.sendEmail(address, title, content) // メール送信
+
+  GmailApp.sendEmail(mailAddress, mailSubject, content)
 }

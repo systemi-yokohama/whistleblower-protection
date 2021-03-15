c83619fba0c56e7cb7b7dfae1d690b98f4130917
Satoshi Ogata <satosystems@gmail.com>
2021-03-15 14:15:26 +0900

    Fix some fixable problems of ESLint
    修正可能は ESLint の警告を改修。

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-function submitForm(e) {
+/* global GmailApp, SpreadsheetApp */
 
-  var s = SpreadsheetApp.openById("1hxE7cBpoPz3o5o2EJtExOCUFH5SFDoOiG-vW1MrJByo").getSheetByName('送信アドレス') //シート取得
-  var data = s.getDataRange().getValues()  //項目・連絡先が入力されているセルの取得
+// eslint-disable-next-line no-unused-vars
+function submitForm (e) {
+  const s = SpreadsheetApp.openById('1hxE7cBpoPz3o5o2EJtExOCUFH5SFDoOiG-vW1MrJByo').getSheetByName('送信アドレス') // シート取得
+  const data = s.getDataRange().getValues() // 項目・連絡先が入力されているセルの取得
 
-  var title = ''
-  var answer = ''
-  var content = ''
-  var address = ''
-  var itemResponse = ''
-  var titles = ''
-  var answers = ''
-  var arr0 = new Array()
-  var arr1 = new Array()
+  let title = ''
+  let answer = ''
+  let content = ''
+  let address = ''
+  let itemResponse = ''
+  let titles = ''
+  let answers = ''
+  const arr0 = []
+  const arr1 = []
 
-  function debug(str) {
-    const ss = SpreadsheetApp.openById("1hxE7cBpoPz3o5o2EJtExOCUFH5SFDoOiG-vW1MrJByo");
-    const s = ss.getSheetByName("デバッグログ");
-    s.appendRow([new Date().toLocaleString(), str]);
+  // eslint-disable-next-line no-unused-vars
+  function debug (str) {
+    const ss = SpreadsheetApp.openById('1hxE7cBpoPz3o5o2EJtExOCUFH5SFDoOiG-vW1MrJByo')
+    const s = ss.getSheetByName('デバッグログ')
+    s.appendRow([new Date().toLocaleString(), str])
   }
 
-  //フォーム入力内容の取得
-  var itemResponses = e.response.getItemResponses()
+  // フォーム入力内容の取得
+  const itemResponses = e.response.getItemResponses()
   for (let i = 0; i < itemResponses.length; i++) {
     itemResponse = itemResponses[i]
     title = itemResponse.getItem().getTitle()
@@ -34,20 +37,17 @@ function submitForm(e) {
     content += answer
   }
 
-  //入力内容によって、取得したセル値から送信先を選択
+  // 入力内容によって、取得したセル値から送信先を選択
   for (let d = 1; d < data.length; d++) {
-    
-    if (arr0[0] == "相談内容を選択して下さい") {
-      if (arr1[0] == "ホットライン") {
+    if (arr0[0] == '相談内容を選択して下さい') {
+      if (arr1[0] == 'ホットライン') {
         address = data[d][1]
         break
-      }
-      else if (arr1[0] == "コンプライアンス") {
+      } else if (arr1[0] == 'コンプライアンス') {
         address = data[d][1]
       }
     }
   }
-  var title = "お問い合わせが送信されました"
-  GmailApp.sendEmail(address, title, content)  //メール送信
+  title = 'お問い合わせが送信されました'
+  GmailApp.sendEmail(address, title, content) // メール送信
 }
-
